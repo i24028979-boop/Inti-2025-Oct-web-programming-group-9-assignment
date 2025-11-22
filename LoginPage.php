e576c8ce6264e43251b02f7d034fe9bd032c7354
i24028979-boop <i24028979@student.newinti.edu.my>
2025-11-22 15:01:06 +0800

    Add files via upload

---
+++ LoginPage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <meta charset="UTF-8">
+  <title>Banking System (group 9)</title>
+</head>
+
+
+<body>
+<div id="header" style="background-color:#B0E0E6;clear:both;text-align:center;">
+<h1>Banking System (group 9)</h1></div>
+
+<div id="login" style="backgroud-color:#B0C4DE;text-align:center;">
+<!--user ID input-->
+<label for="User ID">User ID:</label>
+<input type="text" id="User ID" name="User ID" required><br><br>
+
+<!--password-->
+<label for="Password">Password:</label>
+<input type="password" id="Password" name="Password" required><br><br>
+
+<button>Login</button>
+
+<button>Forgot Password</button>
+
+<button>Register New Account</button><br><br></div>
+
+<div id="footer" style="background-color:#B0E0E6;clear:both;text-align:center;">
+Banking System (group 9)</div>
+
+</body>
